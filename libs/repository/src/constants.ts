352d8d71f6ec76515efea3fe2dd77bfde2f38df3
denlysenko <den.lysenko.mail@gmail.com>
2020-06-25 18:01:59 +0300

    fix collection names

--- libs/repository/src/constants.ts
+++ libs/repository/src/constants.ts
@@ -4,8 +4,8 @@ export const BANK_DETAILS_REPOSITORY = Symbol('BankDetailsRepository');
 export const PAYMENTS_REPOSITORY = Symbol('PaymentsRepository');
 export const ORDERS_REPOSITORY = Symbol('OrdersRepository');
 // Mongoose requires name as string
-export const LOG_MODEL = 'LogModel';
-export const MESSAGE_MODEL = 'MessageModel';
-export const SESSION_MODEL = 'SessionModel';
+export const LOG_MODEL = 'Log';
+export const MESSAGE_MODEL = 'Message';
+export const SESSION_MODEL = 'Session';
 export const SEQUELIZE = Symbol('Sequelize');
 export const MONGO = Symbol('Mongo');
